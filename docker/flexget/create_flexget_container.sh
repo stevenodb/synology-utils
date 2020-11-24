a6c38d0dd4b2f4badd67451ff8ab23379b728f2e
Steven Op de beeck <steven@opdebeeck.org>
2020-11-24 15:46:50 +0100

    Fix wrong combination of docker parameters

--- docker/flexget/create_flexget_container.sh
+++ docker/flexget/create_flexget_container.sh
@@ -1,1 +1,1 @@
-docker run --rm --restart always --name flexget --volume /volume1/docker/flexget:/home/flexget/.flexget --volume /volume1/download/watch:/home/flexget/torrents flexget
+docker run --restart always --name flexget --volume /volume1/docker/flexget:/home/flexget/.flexget --volume /volume1/content/download/watch:/home/flexget/torrents flexget
