04f8e43c279c27f042d44b0f35cb1dc788a1d2fe
Steven Op de beeck <steven@opdebeeck.org>
2021-03-20 15:54:16 +0100

    rebuild docker file, expose ui por

--- docker/flexget/create_flexget_container.sh
+++ docker/flexget/create_flexget_container.sh
@@ -1,1 +1,3 @@
-docker run --restart always --name flexget --volume /volume1/docker/flexget:/home/flexget/.flexget --volume /volume1/content/download/watch:/home/flexget/torrents flexget
+docker pull python:3-slim-buster
+docker build .
+docker run --restart always --name flexget --user 1029 -p 5050:5050 --volume /volume1/docker/flexget/config:/home/flexget/.flexget --volume /volume1/docker/flexget/torrents:/home/flexget/torrents -d flexget
